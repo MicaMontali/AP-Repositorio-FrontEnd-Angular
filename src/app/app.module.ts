247662571e3149b3968bd6c1686c3853ec8d1abb
@MicaMontali <micamontali@gmail.com>
2022-10-04 18:36:36 -0300

    trabajo con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { CarruselComponent } from './componentes/carrusel/carrusel.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarruselComponent,
+    ServiciosComponent,
+    SobreMiComponent,
+    ClientesComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
